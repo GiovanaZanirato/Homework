aab1e9bc1790980b8cc23274bcba7fc9a514b71e
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-05-19 16:20:21 -0300

    Add files via upload

---
+++ giovana_210518.py
@@ -0,0 +1,47 @@
+#question 2
+love = "giovana"
+#question 3
+plant = ("11")
+#question 4
+print(love+str(plant))
+#question 5
+print(love+str(plant)+"!")
+#question 6
+bed = (love+ " " +plant)
+print(bed+ "!")
+#question 7
+name ="giovana"
+lastname ="zanirato"
+complete =(name+ " " +lastname)
+#question 8
+print(complete)
+#question 9
+print(complete.upper())
+#question 10
+print(complete.lower())
+#question 11
+print(complete.title())
+#question 12
+print("giovana\nzanirato")
+#question 13
+print("giovana\n\tzanirato")
+#question 14
+food =("  pizza  ")
+#question 15
+print(food)
+#question 16
+food =("  pizza  ")
+print(food + "!")
+#question 17
+print(food.strip() + "!")
+#question 18
+print(102/23)
+#question 19
+user_clouds=input("what`s your favorite food?")
+print(user_clouds.upper()+ " " +user_clouds.upper()+ " " +user_clouds.upper() + "!")
+#question 20
+print(1+1)
+print("1"+"1")
+print(str(1)+"1")
+print(1+int("1"))
+
